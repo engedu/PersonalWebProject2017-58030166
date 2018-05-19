e830e3a4e1cc2079e4d6b58eb7c1d6cb395099bc
gnkktp <30985235+gnkktp@users.noreply.github.com>
2018-05-19 22:23:07 +0700

    Personal-Project-58030166

---
+++ PersonalProject_Webtech/script/main.js
@@ -0,0 +1,19 @@
+/* define a searchfunction */
+function searchFunction() {
+    var input,filter,figure,a,i; // global variables
+    input = document.getElementById('myinput'); // get an element id="myinput" in DOM HTML
+    filter = input.value.toUpperCase(); // change a character to A-Z when you type a-z
+    article = document.getElementById('front-on-air'); // get an element id="front-actors" in DOM HTML and keep value in article variable
+    figure = article.getElementsByTagName('div'); // get div element in DOM HTML and keep value in figure variable
+
+
+    for(i=0; i< figure.length; i++){  // check a data value in figure variable (value get in DOM HTML)
+        a = figure[i].getElementsByTagName('a')[0];
+        if(a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            figure[i].style.display = ""; // display a data 
+        }
+        else{
+            figure[i].style.display = 'none'; // no keyword not show content
+        }
+    }
+}
